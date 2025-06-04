7f1281f07d9603a1b357f66302ec9b1f9a1bd164
LuizTools <luizfduartejr@gmail.com>
2025-06-03 23:07:12 -0300

    aula 3

--- index.js
+++ index.js
@@ -1,17 +1,47 @@
 const axios = require("axios");
+const crypto = require("crypto");
 
 const SYMBOL = "BTCUSDT";
-const BUY_PRICE = 34160;
-const SELL_PRICE = 34501;
+const QUANTITY = "0.001";
 
 const API_URL = "https://testnet.binance.vision";//https://api.binance.com
+const API_KEY = "XXXXX";
+const SECRET_KEY = "XXXXXX";
 
 let isOpened = false;
 
-function calcSMA(data){
+function calcSMA(data) {
     const closes = data.map(candle => parseFloat(candle[4]));
-    const sum = closes.reduce((a,b) => a + b);
+    const sum = closes.reduce((a, b) => a + b);
     return sum / data.length;
+}
+
+async function newOrder(symbol, quantity, side){
+    const order = { symbol, quantity, side };
+    order.type = "MARKET";
+    order.timestamp = Date.now();
+
+    const signature = crypto
+        .createHmac("sha256", SECRET_KEY)
+        .update(new URLSearchParams(order).toString())
+        .digest("hex");
+
+    order.signature = signature;
+
+    try{
+        const {data} = await axios.post(
+            API_URL + "/api/v3/order", 
+            new URLSearchParams(order).toString(), 
+            {
+                headers: { "X-MBX-APIKEY": API_KEY }
+            }
+        )
+
+        console.log(data);
+    }
+    catch(err){
+        console.error(err.response.data);
+    }
 }
 
 async function start() {
@@ -26,12 +56,12 @@ async function start() {
     console.log("SMA: " + sma);
     console.log("Is Opened? " + isOpened);
 
-    if (price <= (sma * 0.9) && isOpened === false) {
-        console.log("comprar");
+    if (price <= 200000 && isOpened === false) {
         isOpened = true;
+        newOrder(SYMBOL, QUANTITY, "buy");
     }
     else if (price >= (sma * 1.1) && isOpened === true) {
-        console.log("vender");
+        newOrder(SYMBOL, QUANTITY, "sell");
         isOpened = false;
     }
     else
